64c3b08c4f9beecf580549e08cb8ac417c73db36
Jeff Hoffer <jeff.hoffer@rollbar.com>
2021-04-16 15:13:28 -0700

    fix(constants): bug in isValidLevel implementation fixed

--- src/utils.js
+++ src/utils.js
@@ -14,5 +14,6 @@ export function wrapValue(val, defaultAs) {
 }
 
 export function isValidLevel(level) {
-  return VALID_LEVELS[level] >= constants.LEVEL_DEBUG && VALID_LEVELS[level] <= constants.LEVEL_CRITICAL;
+  return VALID_LEVELS[level] >= VALID_LEVELS[constants.LEVEL_DEBUG]
+    && VALID_LEVELS[level] <= VALID_LEVELS[constants.LEVEL_CRITICAL];
 }
