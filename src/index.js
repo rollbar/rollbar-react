877e67cb0f8c7f5c9a463e5f7f3f36a3ead9060d
Walt Jones <waltjones@gmail.com>
2022-03-01 13:18:11 -0800

    feat: export provider context for class components to use (#38)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export { LEVEL_DEBUG, LEVEL_INFO, LEVEL_WARN, LEVEL_ERROR, LEVEL_CRITICAL } from './constants';
 export { historyContext } from './history-context';
-export { Provider } from './provider';
+export { Provider, Context, getRollbarFromContext } from './provider';
 export { ErrorBoundary } from './error-boundary';
 export { RollbarContext } from './rollbar-context';
 export { isValidLevel } from './utils';
