757eefeeaed1d70f65ab08eef7db2b6c98cd60f3
Isaac Snow <isaacjsnow@gmail.com>
2022-03-08 13:06:09 -0600

    nextjs example

---
+++ examples/nextjs/pages/_app.tsx
@@ -0,0 +1,34 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Provider, ErrorBoundary } from '@rollbar/react';
+
+const rollbarConfig = {
+  accessToken: process.env.NEXT_PUBLIC_ROLLBAR_TOKEN,
+  hostSafeList: [
+    'localhost:3000',
+    'localhost:4000',
+  ],
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: 'development',
+    client: {
+      javascript: {
+        code_version: '1.0.0',
+        source_map_enabled: true,
+      }
+    }
+  }
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
+    </Provider>
+  );
+}
+
+export default MyApp
