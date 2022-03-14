e3e26bafbbdde7fb62b771e6c6784d8dcbf56d1b
Walt Jones <waltjones@gmail.com>
2022-03-14 11:22:08 -0400

    test: let propType errors cause test failure

--- src/tests/jest-setup.ts
+++ src/tests/jest-setup.ts
@@ -1,2 +1,9 @@
 import '@testing-library/jest-dom';
 import 'regenerator-runtime';
+
+// Let propType errors cause test failure.
+const originalConsoleError = console.error;
+console.error = (message: string) => {
+  if (/(Failed prop type)/.test(message)) throw new Error(message);
+  originalConsoleError(message);
+};
