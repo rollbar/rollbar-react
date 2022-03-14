2add124cb438651168d8965d06cdce7865b7b9b4
Isaac Snow <isaacjsnow@gmail.com>
2022-03-14 10:53:53 -0500

    Add more props to examples (#47)
    * ErrorBoundary prop examples.
    
    * error boundary
    
    * expand nextjs example

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,5 +1,11 @@
+import { React, useState, useMemo } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
-import { Provider as RollbarProvider, ErrorBoundary, RollbarContext, useRollbar } from '@rollbar/react'
+import {
+  Provider as RollbarProvider,
+  ErrorBoundary,
+  RollbarContext,
+  useRollbar
+} from '@rollbar/react'
 
 import './App.css';
 
@@ -17,28 +23,32 @@ const rollbarConfig = {
       javascript: {
         code_version: '1.0.0',
         source_map_enabled: true,
-      }
-    }
-  }
+      },
+    },
+  },
 };
 
 function App() {
   return (
     <RollbarProvider config={rollbarConfig}>
-      <ErrorBoundary>
-        <RollbarContext context="whole-app">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/a">A</Link>
-              </li>
-              <li>
-                <Link to="/b">B</Link>
-              </li>
-            </ul>
-          </nav>
-          <Router />
-        </RollbarContext>
+      <ErrorBoundary
+        level="critical"
+        errorMessage="example error boundary message"
+        fallbackUI={() => <p style={{ color: 'red' }}>Oops, there was an error.</p>}
+        extra={{ more: 'data' }}
+        callback={() => console.log('an exception was sent to rollbar')}
+      >
+        <nav>
+          <ul>
+            <li>
+              <Link to="/a">A</Link>
+            </li>
+            <li>
+              <Link to="/b">B</Link>
+            </li>
+          </ul>
+        </nav>
+        <Router />
       </ErrorBoundary>
     </RollbarProvider>
   );
@@ -58,17 +68,44 @@ function Router() {
 function RouteA() {
   const rollbar = useRollbar();
 
-  return <div>
-    <h1>A</h1>
-    <button onClick={() => rollbar.info('hello, example')}>send message</button>
-  </div>;
+  const [message, setMessage] = useState('example');
+
+  const fullMessage = useMemo(() => `Hello, ${message}!`, [message]);
+
+  return (
+    <RollbarContext context="/a-context">
+      <h1>A</h1>
+      <p>Message: {fullMessage}</p>
+      <input value={message} onChange={(event) => setMessage(event.target.value)} />
+      <br />
+      <button onClick={() => rollbar.info(`Hello, ${message}.`)}>send message</button>
+    </RollbarContext>
+  );
 }
 
 function RouteB() {
-  return <div>
-    <h1>B</h1>
-    <button onClick={() => {
-        throw new Error('uncaught example');
-    }}>throw uncaught error</button>
-  </div>;
+  const [errorState, setErrorState] = useState({ error: false });
+
+  const updateErrorState = () => {
+    // Use an error state and throw inside render,
+    // because React won't send errors within event handlers
+    // to the error boundary component.
+    setErrorState({
+      error: true
+    });
+  };
+
+  if (errorState.error) {
+    // This uncaught error will be handled by the ErrorBoundary.
+    throw new Error('uncaught test error');
+  }
+
+  return (
+    <RollbarContext context="/b-context">
+      <h1>B</h1>
+      <button id='uncaught-error' onClick={updateErrorState}>
+        Throw Uncaught Error
+      </button>
+    </RollbarContext>
+  );
 }
