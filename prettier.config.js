0a0dd1f23d268226a292d1aaa2fe5b70681a6804
Matias Pequeno <matias.pequeno@gmail.com>
2024-02-24 02:32:24 -0300

    Added prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'always',
+};
