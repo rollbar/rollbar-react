85d8115adfec81f8dcb071a731e03da4082d2859
Matias Pequeno <matias.pequeno@gmail.com>
2024-02-24 17:09:33 -0300

    Fixed eslint errors and warnings and enabled lint ci

--- src/tests/utils/provider-util.tsx
+++ src/tests/utils/provider-util.tsx
@@ -1,14 +1,15 @@
-import React, { ReactElement } from 'react';
+import React, { ReactNode, ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
-import { Provider } from '../rollbar-react';
+import { Provider, ProviderProps } from '../rollbar-react';
 
 export const renderWithProviderProps = (
   ui: ReactElement,
   options: RenderOptions,
-  providerProps: any,
-) => {
-  return render(ui, {
-    wrapper: (props) => <Provider {...props} {...providerProps} />,
+  providerProps: Omit<ProviderProps, 'children'>,
+) =>
+  render(ui, {
+    wrapper: (props: { children?: ReactNode }) => (
+      <Provider {...{ children: props.children ?? <></> }} {...providerProps} />
+    ),
     ...options,
   });
-};
