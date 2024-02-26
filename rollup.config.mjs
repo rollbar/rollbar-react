b237de156f46043eadd9d471add4da95e940c95f
Matias Pequeno <matias.pequeno@gmail.com>
2024-02-26 09:28:51 -0300

    Updated deps to latest, typescript ^5, react ^18
    1249 => 929 packages
    8 vulnerabilities (4 moderate, 3 high, 1 critical) => 0 vulnerabilities

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import babel from '@rollup/plugin-babel'
-import pkg from './package.json'
+import pkg from './package.json' assert { type: 'json' }
 
 const COMMON_PLUGINS = [
   resolve(),
