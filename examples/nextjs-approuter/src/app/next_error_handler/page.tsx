5f8f5e78557e0ed7818b16c279660a2cc7e10637
Matthew LaForest <matt.laforest@rollbar.com>
2024-03-13 10:57:25 -0400

    chore(nextjs-approuter): Fix filenames

--- examples/nextjs-approuter/src/app/next_error_handler/page.tsx
+++ examples/nextjs-approuter/src/app/next_error_handler/page.tsx
@@ -1,8 +1,10 @@
+'use client';
+
 export default function NextErrorHandlerGenerator() {
   return (
     <button
       onClick={() => {
-        throw new Error('Something broke');
+        throw new Error('Something handled by NextJS');
       }}
     >
       Click for Error with Next.js Error Handler
