20e882173cb90a3a99fd42176c5e31fbd638b26a
Matthew LaForest <matt.laforest@rollbar.com>
2024-03-15 13:06:33 -0400

    fix(nextjs-approuter): use correct token serverside

--- examples/nextjs-approuter/src/rollbar.ts
+++ examples/nextjs-approuter/src/rollbar.ts
@@ -3,7 +3,7 @@ import Rollbar from 'rollbar';
 export const rollbarInstance = new Rollbar({
   accessToken:
     typeof window === 'undefined'
-      ? process.env.NEXT_PUBLIC_POST_CLIENT_ITEM_TOKEN
+      ? process.env.POST_SERVER_ITEM_TOKEN
       : process.env.NEXT_PUBLIC_POST_CLIENT_ITEM_TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true,
