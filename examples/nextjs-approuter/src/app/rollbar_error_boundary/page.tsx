f6d5db99f9b1adbdf6d12423fe90d95edc39e7ad
Matthew LaForest <matt.laforest@rollbar.com>
2024-03-15 13:54:37 -0400

    chore(nextjs-approuter): houst provider to root layout

--- examples/nextjs-approuter/src/app/rollbar_error_boundary/page.tsx
+++ examples/nextjs-approuter/src/app/rollbar_error_boundary/page.tsx
@@ -1,22 +1,18 @@
-'use client';
-import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import { ErrorBoundary } from '@rollbar/react';
 
-import { rollbarInstance } from '../../rollbar';
 import { ResetPage } from '@/components/ResetPage';
 
 export default function RollbarErrorBoundary() {
   return (
-    <RollbarProvider instance={rollbarInstance}>
-      <ErrorBoundary fallbackUI={() => <ResetPage reset={() => {}} />}>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => {
-            throw new Error('Something broke');
-          }}
-        >
-          Click for Error with Rollbar Error Boundary
-        </button>
-      </ErrorBoundary>
-    </RollbarProvider>
+    <ErrorBoundary fallbackUI={() => <ResetPage reset={() => {}} />}>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => {
+          throw new Error('Something broke');
+        }}
+      >
+        Click for Error with Rollbar Error Boundary
+      </button>
+    </ErrorBoundary>
   );
 }
