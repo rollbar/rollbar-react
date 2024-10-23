6ab4fa70bbce8828acb8fca3326f6b65c6b43a26
Matthew LaForest <matt.laforest@rollbar.com>
2024-10-23 18:34:40 -0400

    chore(examples): fix random indentation change

--- examples/nextjs-approuter/src/components/ResetPage.tsx
+++ examples/nextjs-approuter/src/components/ResetPage.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 export function ResetPage({ reset }: { reset: () => void }) {
-    return (
-        <div>
-            <h1>Something went wrong!</h1>
-            <button onClick={reset}>Try again</button>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Something went wrong!</h1>
+      <button onClick={reset}>Try again</button>
+    </div>
+  );
 }
